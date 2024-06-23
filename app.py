a6fdb5b14824ba76cad7cc555a822c08e2738a91
aisalas <135497533+aisalas@users.noreply.github.com>
2024-06-23 09:32:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pickle
+import numpy as np
 
-st.title('Hello, Streamlit!')
-st.write('This is a simple Streamlit app deployed on Streamlit Cloud.')
+# Load the model
+with open('model_prediksi.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+st.title("Prediksi Produksi Padi")
+
+Luas_panen = st.number_input("Luas Panen")
+Curah_hujan = st.number_input("Curah hujan")
+Kelembapan = st.number_input("Kelembapan")
+Suhu_ratarata = st.number_input("Suhu rata-rata")
+
+if st.button("Prediksi"):
+    features = np.array([[Luas_panen, Curah_hujan, Kelembapan, Suhu_ratarata]])
+    prediction = model.predict(features)
+    st.write(f"Produksi Padi yang Diprediksi: {prediction[0]:.2f}")
+
