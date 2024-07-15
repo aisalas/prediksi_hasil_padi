80d834868910581e5ecb89431882665340e61234
aisalas <135497533+aisalas@users.noreply.github.com>
2024-07-15 14:50:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ st.title("Prediksi Produksi Padi")
 # Luas Panen dengan batasan minimal dan maksimal, dan satuan hektar
 Luas_panen = st.number_input("Luas Panen (hektar)", min_value=0.0, max_value=1000000.0, format="%.2f")
 # Curah Hujan dengan satuan mm
-Curah_hujan = st.number_input("Curah hujan (mm)", format="%.2f")
+Curah_hujan = st.number_input("Curah hujan (mm)", min_value=0.0, max_value=5000.0, format="%.2f")
 # Kelembapan dengan satuan persen
-Kelembapan = st.number_input("Kelembapan (%)", format="%.2f")
+Kelembapan = st.number_input("Kelembapan (%)", min_value=0.0, max_value=100.0, format="%.2f")
 # Suhu Rata-rata dengan satuan derajat Celsius
-Suhu_ratarata = st.number_input("Suhu rata-rata (°C)", format="%.2f")
+Suhu_ratarata = st.number_input("Suhu rata-rata (°C)", max_value=100.0, format="%.2f")
 
 if st.button("Prediksi"):
     features = np.array([[Luas_panen, Curah_hujan, Kelembapan, Suhu_ratarata]])
